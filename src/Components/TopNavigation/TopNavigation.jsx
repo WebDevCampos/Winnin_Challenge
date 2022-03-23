0c537d650aacd096d20c903cea5b049e531d345d
WebDevCampos <webdevcampos@gmail.com>
2022-03-23 15:03:18 -0300

    Changes

--- src/Components/TopNavigation/TopNavigation.jsx
+++ src/Components/TopNavigation/TopNavigation.jsx
@@ -7,23 +7,22 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function TopNavigation() {
   return (
     <Router>
-      <div className="d-flex justify-content-center align-items-center p-3 topNavigation">
-        <Link
-          to="/"
-          className="linkToPage"
-          onClick={(e) => {
-            console.log(e.target);
-          }}
-        >
-          <Button categorie="Hot" />
-        </Link>
-        <Link to="news" className="linkToPage">
-          <Button categorie="News" />
-        </Link>
-        <Link to="rising" className="linkToPage">
-          <Button categorie="Rising" />
-        </Link>
-      </div>
+      <Link
+        to="/"
+        className="linkToPage"
+        onClick={(e) => {
+          console.log(e.target);
+        }}
+      >
+        <Button categorie="Hot" />
+      </Link>
+      <Link to="news" className="linkToPage">
+        <Button categorie="News" />
+      </Link>
+      <Link to="rising" className="linkToPage">
+        <Button categorie="Rising" />
+      </Link>
+
       <Routes>
         <Route exact path="/" element={<Hot />} />
         <Route path="/news" element={<News />} />
