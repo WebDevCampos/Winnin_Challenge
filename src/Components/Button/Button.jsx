348e44ff851a86717ded00492fe83a1c0473ddf2
WebDevCampos <webdevcampos@gmail.com>
2022-03-23 15:12:42 -0300

    Changes

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,9 +1,5 @@
 import "./Button.css";
 function Button({ categorie }) {
-  return (
-    <div className="activeCategorie categorieButton  mx-3 mx-md-1">
-      {categorie}
-    </div>
-  );
+  return <div className="activeCategorie categorieButton  ">{categorie}</div>;
 }
 export default Button;
