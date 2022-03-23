fb179edfeb95e03150c6d50e88c1cf88d0057e30
WebDevCampos <webdevcampos@gmail.com>
2022-03-23 14:53:47 -0300

    Changes

--- src/Components/TopNavigation/TopNavigation.jsx
+++ src/Components/TopNavigation/TopNavigation.jsx
@@ -7,31 +7,27 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function TopNavigation() {
   return (
     <Router>
-      <div className="container ">
-        <div className="d-flex justify-content-center align-items-center p-3">
-          <Link
-            to="/"
-            className="linkToPage"
-            onClick={(e) => {
-              console.log(e.target);
-            }}
-          >
-            <Button categorie="Hot" />
-          </Link>
-          <Link to="news" className="linkToPage">
-            <Button categorie="News" />
-          </Link>
-          <Link to="rising" className="linkToPage">
-            <Button categorie="Rising" />
-          </Link>
-        </div>
+      <div className="container d-flex justify-content-center align-items-center p-3">
+        <Link
+          to="/"
+          className="linkToPage"
+          onClick={(e) => {
+            console.log(e.target);
+          }}
+        >
+          <Button categorie="Hot" />
+        </Link>
+        <Link to="news" className="linkToPage">
+          <Button categorie="News" />
+        </Link>
+        <Link to="rising" className="linkToPage">
+          <Button categorie="Rising" />
+        </Link>
       </div>
       <Routes>
-        <div className="container  d-flex justify-content-center align-items-center p-3">
-          <Route exact path="/" element={<Hot />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/rising" element={<Rising />} />
-        </div>
+        <Route exact path="/" element={<Hot />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/rising" element={<Rising />} />
       </Routes>
     </Router>
   );
