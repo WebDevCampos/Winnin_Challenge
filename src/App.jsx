deeaba6980c00754cd08da871d565f6790e654ce
WebDevCampos <webdevcampos@gmail.com>
2022-03-25 16:40:44 -0300

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import Header from "./Components/Header/Header";
 import TopNavigation from "./Components/TopNavigation/TopNavigation";
-import Footer from "./Components/Footer/Footer";
-import Hot from "./Pages/Hot/Hot";
-import { Button } from "./StyledComponents/Style";
 
 import "./App.css";
-
 function App() {
   return (
     <div className="App">
