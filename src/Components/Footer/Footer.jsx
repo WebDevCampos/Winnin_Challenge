7d6c0e8ed5f7a48b2b8b829f14bd8b2125c11b57
Marcus Vinicius <webdevcampos@gmail.com>
2022-04-29 11:21:40 -0300

    changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,10 +5,9 @@ function Footer() {
     <footer
       className="container d-flex justify-content-center align-items-center mainFooter  bottom-0"
       onClick={() => {
-        fetch("https://www.reddit.com/r/reactjs.json")
-          .then((res) => res.json())
-          .then((data) => setContent(data.data.children))
-          .catch((e) => console.log(e));
+        document.querySelector("main").style.overflow = "visible";
+        document.querySelector("main").style.height = "auto";
+        scroll(0, 500);
       }}
     >
       + Ver Mais
